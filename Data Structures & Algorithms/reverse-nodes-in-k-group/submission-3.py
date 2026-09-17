180bd787a78c44466eeadc7cf37a2867d1cfe6ac
Yaroslav Bezvesilnyi <yaroslavbezvesilnyi@gmail.com>
2026-09-17 10:44:44 +0100

    Add: reverse-nodes-in-k-group - submission-3

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-3.py
@@ -0,0 +1,73 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        #1st approach
+        # count = k
+        # if not head or not head.next:
+        #     return head
+        # dummy = ListNode(0, head)
+        # curr = head
+        # prev = None
+        # currHead = head
+        # prevHead = dummy
+        # while curr != None:
+        #     if count == k:
+        #         if curr != head:
+        #             prevHead = currHead
+        #             currHead = curr
+        #         test = curr
+        #         currCount = count
+        #         while test != None and currCount > 0:
+        #             currCount -= 1
+        #             test = test.next
+        #         if currCount:
+        #             prevHead.next = curr
+        #             break
+        #     if count <= 1:
+        #         temp = curr.next
+        #         curr.next = prev
+        #         prev = None
+        #         prevHead.next = curr
+        #         curr = temp
+        #         count = k
+        #     else:
+        #         temp = curr.next
+        #         curr.next = prev
+        #         prev = curr
+        #         curr = temp
+        #         count -= 1
+        # return dummy.next
+
+        #2nd approach
+
+        dummy = ListNode(0)
+        prevHead = dummy
+        curr = head
+        while curr != None:
+            t = curr
+            n1 = curr
+            count = k
+            while curr != None and count > 0:
+                curr = curr.next
+                count -= 1
+            n2 = curr
+            if count == 0:
+                prev = None
+                while n1 != n2:
+                    temp = n1.next
+                    n1.next = prev
+                    prev = n1
+                    n1 = temp
+                prevHead.next = prev
+                prevHead = t 
+            else:
+                prevHead.next = n1
+                break
+        return dummy.next
+
+        
